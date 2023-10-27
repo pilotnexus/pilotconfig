7c88d0f4fdcdde60710bd4d77ba56fba7296194c
Daniel Amesberger <daniel.amesberger@amescon.com>
2023-10-27 09:57:25 +0200

    improved install script for raspberry pi

--- install-pilot-config-for-raspberry-pi.sh
+++ install-pilot-config-for-raspberry-pi.sh
@@ -5,30 +5,43 @@ if ! command -v pipx &> /dev/null; then
     echo "pipx not found! Installing pipx..."
     python3 -m pip install --user pipx
     python3 -m pipx ensurepath
-    # Reload shell to ensure new path is active
-    source ~/.bashrc
+
+    # Update PATH directly for the current session to include pipx bin, in case .bashrc or .profile changes aren't sourced immediately
+    export PATH="$PATH:$HOME/.local/bin"
 fi
 
-# Install your tool with pipx
-pipx install pilot-config
+PILOT_PATH="$HOME/.local/bin/pilot"
+PILOT_BACKUP="$PILOT_PATH"_original
 
-# Backup the original pilot script
-cp ~/.local/bin/pilot ~/.local/bin/pilot_original
+# Always force reinstall 'pilot-config' to get the latest version and to avoid file collisions
+pipx install pilot-config --force
 
-# Insert the privilege escalation code right after the shebang
-sed '1a\
+# Check if the 'pilot' binary is present and non-empty
+if [ ! -f $PILOT_PATH ] || [ ! -s $PILOT_PATH ]; then
+    echo "Error: Unable to properly install or locate the pilot binary."
+    exit 1
+fi
+
+# Check if the privilege escalation code is already present
+if ! grep -q "Re-run the script with sudo" $PILOT_PATH; then
+    # Backup the original pilot script
+    cp $PILOT_PATH $PILOT_BACKUP
+
+    # Insert the privilege escalation code right after the shebang
+    sed '1a\
 import os, sys\
 \
 # Check if we are running as root already.\
 if os.geteuid() != 0:\
     # Re-run the script with sudo\
     os.execvp("sudo", ["sudo", sys.executable] + sys.argv)\
-' ~/.local/bin/pilot_original > ~/.local/bin/pilot
+' $PILOT_BACKUP > $PILOT_PATH
 
-# Remove the backup
-rm ~/.local/bin/pilot_original
+    # Remove the backup
+    rm $PILOT_BACKUP
+fi
 
 # Ensure that the pilot script is executable
-chmod +x ~/.local/bin/pilot
+chmod +x $PILOT_PATH
 
 echo "Installation complete! Start using pilot-config by typing 'pilot setup'"
